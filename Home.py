3506f79e5270b76acc04445c4be031fcb2aa3d27
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-01 20:19:35 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("Hey")
+import pandas as pd
+import json
+st.set_page_config(layout="wide")
+
+st.title("ChangeEngine Content AI")
+st.subheader("Version 1")
+st.write("The purpose of this micro-app is to create content to fill the templates in the PCC AI Airtable base.")
