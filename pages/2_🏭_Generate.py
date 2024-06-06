e53b22c5e6112c5f3019fefbe4df6cbb19c20298
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-05 18:20:24 -0700

    Update 2_🏭_Generate.py

--- pages/2_🏭_Generate.py
+++ pages/2_🏭_Generate.py
@@ -133,8 +133,6 @@ if selected_content_type != "Select a Content Type":
         if st.button("Generate"):
             # Generate prompts array
             prompts_array = generate_prompts_array(image_prompt, layouts_array)
-            st.write("Debug Prompt Array")
-            st.write(prompts_array)
 
             # Go to OpenAI for each one
             n = 1
