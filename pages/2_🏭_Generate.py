2c8e5b861c3c62a68728eff81891634dd7ca61d1
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-11 14:12:45 -0700

    Update 2_🏭_Generate.py

--- pages/2_🏭_Generate.py
+++ pages/2_🏭_Generate.py
@@ -46,10 +46,10 @@ topic = st.text_input("Content", "Zoom yoga session tomorrow Friday June 7")
 selected_content_type = st.selectbox("Choose a Content Type", options)
 
 # Create an input box for company name
-company_name = st.text_input("Company Name", "NewCo")
+company_name = st.text_input("Company Name", "Pull in Andela")
 
 # Create an input box for company name
-test_description = "Test company description goes here"
+test_description = "You need to pull this in from the Airtable table Client AI + Automation"
 company_tone_style = st.text_area("Company Tone and Style Guide", value=test_description, height=200)
 
 # Text input for layouts (comma-separated integers)
