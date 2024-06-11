43a83d3a0a79ef5242dcfbf2ada0621388cbd26a
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-11 14:08:03 -0700

    Update 2_🏭_Generate.py

--- pages/2_🏭_Generate.py
+++ pages/2_🏭_Generate.py
@@ -48,6 +48,9 @@ selected_content_type = st.selectbox("Choose a Content Type", options)
 # Create an input box for company name
 company_name = st.text_input("Company Name", "NewCo")
 
+# Create an input box for company name
+company_tone_style = st.text_input("Company Tone and Style Guide", "Tone Guide goes here", height=200)
+
 # Text input for layouts (comma-separated integers)
 selected_layouts = st.text_input("Select Layouts", "1, 3")
 
