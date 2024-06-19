fdd91cb1cce2851080c7c94f62a6f0b540fd1258
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-18 22:33:47 -0700

    Create 4_scratch.py

---
+++ pages/4_scratch.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+draft_response = """Sure, here are the five different variant drafts for the LinkedIn banner:
+
+-------
+Title:
+Join Us!
+Build Tech
+With Us
+
+Hashtag 1: #HiringSoftwareDevs
+Suggest Photo/Illustration/Icon: a cartoon illustration
+Hashtag 2: #JoinOurTechTeam
+-------
+
+-------
+Title:
+Code Now,
+Grow Now
+Join Us
+
+Hashtag 1: #HiringSoftwareDevs
+Suggest Photo/Illustration/Icon: a cartoon illustration
+Hashtag 2: #TechCareersHere
+-------
+
+-------
+Title:
+Dev Team
+Needs You!
+Apply Now
+
+Hashtag 1: #JoinOurTechTeam
+Suggest Photo/Illustration/Icon: a cartoon illustration
+Hashtag 2: #SoftwareJobsOpen
+-------
+
+-------
+Title:
+Dream Job,
+Code Now,
+Succeed
+
+Hashtag 1: #NowHiringDevs
+Suggest Photo/Illustration/Icon: a cartoon illustration
+Hashtag 2: #TechCareersHere
+-------
+
+-------
+Title:
+Hiring Now!
+Developers
+Welcome!
+
+Hashtag 1: #TechCareersHere
+Suggest Photo/Illustration/Icon: a cartoon illustration
+Hashtag 2: #JoinOurTechTeam
+-------
+
+Hope these meet your requirements!"""
+
