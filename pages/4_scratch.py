7e9fc5be59fb08db012c1bc4a2629a0caf8bf3ee
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-18 22:37:03 -0700

    Update 4_scratch.py

--- pages/4_scratch.py
+++ pages/4_scratch.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from helpers import send_to_openai_with_tools, tools, extract_key_value_pairs
 
 draft_response = """Sure, here are the five different variant drafts for the LinkedIn banner:
 
@@ -59,3 +60,11 @@ Hashtag 2: #JoinOurTechTeam
 
 Hope these meet your requirements!"""
 
+response = st.text_area(value=draft_response, height=200)
+
+if st.button("Generate")"
+    tool_call_prompt = "Please extract relevant entities (Title, Subtitle and any others) from the below text." + "\n\n---------------\n\n" + response
+    layout_messages = [{"role": "user", "content": tool_call_prompt}]
+    layout_response = send_to_openai_with_tools(layout_messages)
+    pairs_json = extract_key_value_pairs(layout_response)
+    st.write(pairs_json)
