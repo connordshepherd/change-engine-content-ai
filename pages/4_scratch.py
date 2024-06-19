ff49edc74386634ec2e26385a3fd9a7d74d8c60d
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-18 22:53:19 -0700

    Update 4_scratch.py

--- pages/4_scratch.py
+++ pages/4_scratch.py
@@ -60,11 +60,75 @@ Hashtag 2: #JoinOurTechTeam
 
 Hope these meet your requirements!"""
 
+pairs_json = [
+  {
+    "key": "TITLE",
+    "value": "Join Us\nIn Our\nMission"
+  },
+  {
+    "key": "HASHTAG 1",
+    "value": "#SoftwarePlusSolutions"
+  },
+  {
+    "key": "HASHTAG 2",
+    "value": "#DevelopWithUsNow"
+  },
+  {
+    "key": "TITLE",
+    "value": "Code It\nWith Us\nToday!"
+  },
+  {
+    "key": "HASHTAG 1",
+    "value": "#TechJobsAvailableNow"
+  },
+  {
+    "key": "HASHTAG 2",
+    "value": "#SoftwareCareersOpen"
+  },
+  {
+    "key": "TITLE",
+    "value": "Join Our\nDev Team\nNow!"
+  },
+  {
+    "key": "HASHTAG 1",
+    "value": "#InnovateWithCode"
+  },
+  {
+    "key": "HASHTAG 2",
+    "value": "#JoinOurDevTeam"
+  },
+  {
+    "key": "TITLE",
+    "value": "We Are\nHiring\nNow!"
+  },
+  {
+    "key": "HASHTAG 1",
+    "value": "#DevelopAtBestPlace"
+  },
+  {
+    "key": "HASHTAG 2",
+    "value": "#JoinOurTalentedTeam"
+  },
+  {
+    "key": "TITLE",
+    "value": "Your New\nCareer\nStarts"
+  },
+  {
+    "key": "HASHTAG 1",
+    "value": "#CodeYourFuture"
+  },
+  {
+    "key": "HASHTAG 2",
+    "value": "#DevelopCareersHere"
+  }
+]
+
 response = st.text_area("Response", value=draft_response, height=200)
 
 if st.button("Generate"):
-    tool_call_prompt = "Please extract relevant entities (Title, Subtitle and any others) from the below text." + "\n\n---------------\n\n" + response
-    layout_messages = [{"role": "user", "content": tool_call_prompt}]
-    layout_response = send_to_openai_with_tools(layout_messages)
-    pairs_json = extract_key_value_pairs(layout_response)
-    st.write(pairs_json)
+    # Assemble pairs_json unless you already have it
+    # tool_call_prompt = "Please extract relevant entities (Title, Subtitle and any others) from the below text." + "\n\n---------------\n\n" + response
+    #layout_messages = [{"role": "user", "content": tool_call_prompt}]
+    #layout_response = send_to_openai_with_tools(layout_messages)
+    #pairs_json = extract_key_value_pairs(layout_response)
+    #st.write(pairs_json)
