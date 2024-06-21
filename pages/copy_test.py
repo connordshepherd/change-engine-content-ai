a68b008bf32459e21cf217cfa5cd75111fd116e0
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-21 14:49:25 -0700

    Update copy_test.py

--- pages/copy_test.py
+++ pages/copy_test.py
@@ -1,21 +1,5 @@
 import streamlit as st
+from st_copy_to_clipboard import st_copy_to_clipboard
 
-def create_copy_button(text_to_copy):
-    button_id = "copyButton" + text_to_copy
-    
-    button_html = f"""<button id="{button_id}">Copy</button>
-    <script>
-    document.getElementById("{button_id}").onclick = function() {{
-        navigator.clipboard.writeText("{text_to_copy}").then(function() {{
-            console.log('Async: Copying to clipboard was successful!');
-        }}, function(err) {{
-            console.error('Async: Could not copy text: ', err);
-        }});
-    }}
-    </script>"""
-    
-    st.markdown(button_html, unsafe_allow_html=True)
-
-text_to_copy = "Hello, Streamlit!"
-st.text_input("Text to copy:", value=text_to_copy, key="text_to_copy")
-create_copy_button(st.session_state.text_to_copy)
+st.title("Copy to Clipboard Test")
+st_copy_to_clipboard("Test text to copy")
