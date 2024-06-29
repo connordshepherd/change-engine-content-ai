46638d1bba7da6a00a146e5e4827486447f56428
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-06-29 00:08:55 -0700

    Update 5_v3_UI.py

--- pages/5_v3_UI.py
+++ pages/5_v3_UI.py
@@ -16,6 +16,14 @@ from typing import List, Dict, Union, Any, Tuple
 import webbrowser
 from streamlit.components.v1 import html
 
+def open_page(url):
+    open_script = """
+    <script type="text/javascript">
+        window.open('%s', '_blank').focus();
+    </script>
+    """ % url
+    html(open_script)
+
 # Streamlit Widescreen Mode
 st.set_page_config(layout="wide")
 
