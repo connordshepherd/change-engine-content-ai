a1733dbccf0f6cc0ae48dfdf77b6f3cb58e5b299
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-09-08 21:18:43 -0700

    Update full_blueprint_json.py

--- pages/full_blueprint_json.py
+++ pages/full_blueprint_json.py
@@ -93,7 +93,7 @@ def process_prompts(pcc_plaintext):
 
     st.write("Running prompt 3 - Adding Educational Elements")
     # Process prompt 3
-    full_prompt_3 = prompt_3_boilerplate + '\n\n' + "As a reminder, the JSON object with steps and elements we're adding to is:" + \n\n + str(response_2)
+    full_prompt_3 = prompt_3_boilerplate + '\n\n' + "As a reminder, the JSON object with steps and elements we're adding to is:" + '\n\n' + str(response_2)
     messages.append({"role": "user", "content": full_prompt_3})
     response_3 = call_openai_with_tools(messages, tools)
     messages.append({"role": "assistant", "content": response_3})
