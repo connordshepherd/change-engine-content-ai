4207a0cbcf5592c6657655ace8d81e51f0cf9818
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-09-25 20:16:28 -0700

    Update full_blueprint_2.py

--- pages/full_blueprint_2.py
+++ pages/full_blueprint_2.py
@@ -164,9 +164,10 @@ Recognize the collective efforts and accomplishments of employees, while ensurin
 You're creating the plan, so the first step shouldn't be anything like "Create a Plan" or "Define the steps." That's what you're doing!"""
 
 prompt_2_boilerplate = """Great! Now we're going to begin adding elements to each step.\n
-Here are some previous blueprints you can use as examples. See how the elements nest within the steps? Every step should have at least 4 elements. 2 of them should be Communications.\n
+Here are some previous blueprints you can use as examples. See how the elements nest within the steps? Every step should have at least 4 elements.\n
 Don't add Educational Elements yet - we'll worry about that later.\n
-Very important - please only use each element ONCE in your plan - if you use an element in one step you can't use it in other steps.\n"""
+2 elements of each step should always be Communications.\n
+Very important - except for Communication, please only use each element type ONCE in your plan - if you use an element in one step you can't use it in other steps. For example if you use Zoom Background in one step, you can't use any more Zoom Backgrounds in subsequent steps.\n"""
 
 prompt_3_boilerplate = """Great! Now, we need to add in “Educational Elements.” These are places in the plan where the HR lead needs to gather information, circulate information, or define their goals.\n
 Here’s the menu of Educational Elements: \n
